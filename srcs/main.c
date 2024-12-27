381fa302b23df795675e0e08de82fa8fb01348fa
petitcoeur <petitcoeur@petitcoeur.home>
2024-12-27 13:14:49 +0100

    fix error parse_and_execute

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: petitcoeur <petitcoeur@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 11:06:43 by yaabdall          #+#    #+#             */
-/*   Updated: 2024/12/27 12:47:49 by petitcoeur       ###   ########.fr       */
+/*   Updated: 2024/12/27 13:12:51 by petitcoeur       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,11 @@ static void	parse_and_execute(t_minishell *data)
 	init_ignore_signal();
 	ast_root = parse_expression(&i, data);
 	if (g_signal_received == 0)
-	{
 		execute_ast(ast_root, data, false);
-		if (data->tmp_fd != -1)
-		{
-			safe_close(&data->tmp_fd);
-			unlink(data->tmp_file);
-		}
+	if (data->tmp_fd != -1)
+	{
+		safe_close(&data->tmp_fd);
+		unlink(data->tmp_file);
 	}
 	gc_cleanup_except_locked(&data->gc);
 	ast_root = NULL;
